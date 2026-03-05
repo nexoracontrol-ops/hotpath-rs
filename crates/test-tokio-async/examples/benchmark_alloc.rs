04e28b25668de298c800ff009349a89d08f2c7f2
pawurb <contact@pawelurbanek.com>
2026-03-05 21:56:38 +0100

    chore: Update benchmark_alloc

--- crates/test-tokio-async/examples/benchmark_alloc.rs
+++ crates/test-tokio-async/examples/benchmark_alloc.rs
@@ -1,24 +1,29 @@
-use std::time::{Duration, Instant};
-
-const WAIT_FOR: Duration = Duration::from_micros(1);
-
-// This function is used to spin-wait for a given duration.
-// It's more precise than std::thread::sleep.
-fn spin_wait() {
-    let start = Instant::now();
-    while start.elapsed() < WAIT_FOR {}
-}
-
+use std::thread;
 #[hotpath::measure]
 fn alloc() {
-    let vec = vec![1u8; 128];
-    std::hint::black_box(vec);
-    spin_wait();
+    for _ in 0..1000 {
+        let vec = vec![1u8; 128];
+        std::hint::black_box(vec);
+    }
 }
 
 #[hotpath::main]
 fn main() {
-    for _ in 0..100_000 {
-        alloc();
+    let num_threads = std::env::var("HOTPATH_ALLOC_NUM_THREADS")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(2);
+    let handles: Vec<_> = (0..num_threads)
+        .map(|_| {
+            thread::spawn(|| {
+                for _ in 0..100_000 {
+                    alloc();
+                }
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
